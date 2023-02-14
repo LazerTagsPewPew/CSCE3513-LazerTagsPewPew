9c92224ce1958e8620abf3c133490476a070f700
Clayton Warstler <claywarstler@gmail.com>
2023-02-14 14:33:50 -0600

    Created database.java

---
+++ src/main/java/teamseven/lasertag/Database.java
@@ -0,0 +1,25 @@
+package src.main.java.teamseven.lasertag;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    private final String url = "db.yfxjoxxaxhaqikyecrro.supabase.co";
+    private final String userName = "postgres";
+    private final String passWord = "LazerTagPewPew";
+
+    public Connection connect() {
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(url, userName, passWord);
+            System.out.println("Connected to the database...");
+        }
+        catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return connection;
+    }
+}
