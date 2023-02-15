259e90b82a1b60a6515be80eeedf354799ad5d52
Drew Stull <99615718+DrewStull@users.noreply.github.com>
2023-02-14 21:56:31 -0600

    This is the actual playerEntry screen we'll use (2/14/2023)
    all the functionality of the textFields are in place so once the database is linked and we have a member variable of the database in our EntryScreen class, we'll just have to add either DB.querry() or DB.createRecord() to each textFields action listner depending on if its the UserId or codeName textField.

---
+++ src/main/java/teamseven/lasertag/LaserTag.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package teamseven.lasertag;
+
+/**
+ *
+ * @author dmr019
+ * @author drstull
+ */
+public class LaserTag {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(SplashScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(SplashScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(SplashScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(SplashScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        SplashScreen splashScreen = new SplashScreen();
+        EntryTerminal entryTerminal = new EntryTerminal();
+        
+        /* Create and display the form */
+        // make splashScreen visible
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                splashScreen.setVisible(true);
+            }
+        });
+        
+        // display splash screen for 3 seconds
+        try
+        {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e)
+        {
+        }
+        
+        splashScreen.dispose();
+        
+        // make entryTerminal visible
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                entryTerminal.setVisible(true);
+            }
+        });
+        
+    }
+    
+}
